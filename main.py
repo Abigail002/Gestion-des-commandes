a86b48ef96be47d3f74b4547f5b7e4abb377dcaf
Abigail002 <abigailameogno98@gmail.com>
2023-02-16 09:36:34 +0000

    Add of update and delete routes and functions for item table

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from app import app
 from config import db
-from models import Credit, Item, Order, Customer, Order_detail,Payment, Cash, Check, Credit, Wire_transfer#, Order_status
+from models import Item, Order, Customer, Order_detail, Payment, Cash, Check, Credit, Wire_transfer  # , Order_status
+from models.Item import Item
+from models.Order import Order
+from models.Customer import Customer
+from models.Order_detail import Order_detail
+from models.Payment import Payment
+from models.Credit import Credit
+from models.Cash import Cash
+from models.Check import Check
+from models.Wire_transfer import Wire_transfer
 from flask import jsonify, render_template, request
 
 with app.app_context():
-    db.drop_all()
+    # db.drop_all()
     db.create_all()
 
 """CRUD operations for item"""
+
+
 @app.route('/item/add', methods=['POST'])
 def itemAdd():
     try:
@@ -32,11 +43,13 @@ def itemAdd():
         db.session.rollback()
         db.session.close()
 
+
 @app.route('/items', methods=['GET'])
 def items():
     try:
-        items = Item.Item.query.all()
-        data = [{"weight": item.weight, "description": item.description} for item in items]
+        items = Item.query.all()
+        data = [{"weight": item.weight, "description": item.description}
+                for item in items]
         print(data)
         response = jsonify({"statut_code": 200, "items": data})
         return response
@@ -44,6 +57,54 @@ def items():
         print(e)
         message = {'status': 404, 'message': e}
         return message
+
+
+@app.route('/item/update', methods=['PUT'])
+def itemUpdate():
+    try:
+        data = request.json
+        print(data)
+        id = data["id"]
+        weight = data["weight"]
+        description = data["description"]
+        item = Item.query.filter_by(id=id).first()
+        print(item)
+        if id and weight and description and request.method == 'PUT':
+            item.weight = weight
+            item.description = description
+            db.session.commit()
+            response = jsonify(
+                'Les informations de l"item ont été modifiées')
+            return response
+    except Exception as e:
+        print(e)
+        message = {'status': 404, 'message': e}
+        return message
+    finally:
+        db.session.rollback()
+        db.session.close()
+
+
+@app.route('/item/delete', methods=['DELETE'])
+def deleteItem():
+    try:
+        json = request.json
+        print(json)
+        id = json['id']
+
+        item = Item.query.filter_by(id=id).first()
+        print(item)
+        db.session.delete(item)
+        db.session.commit()
+        resultat = jsonify('Item supprimé ♠♦')
+        return resultat
+    except Exception as e:
+        print(e)
+        message = {'status': 404, 'message': e}
+        return message
+    finally:
+        db.session.rollback()
+        db.session.close()
 
 
 if (__name__ == '__main__'):

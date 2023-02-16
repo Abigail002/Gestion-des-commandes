3bf3797c2e803e498b23a1ee4e61d1a64edc33a5
Abigail002 <abigailameogno98@gmail.com>
2023-02-16 09:35:40 +0000

    Add of OrderId foreign key

--- models/Payment.py
+++ models/Payment.py
@@ -5,11 +5,9 @@ class Payment(db.Model):
     amount = db.Column(db.Float)
     payment_mode = db.Column(db.String(255), nullable=False)
 
+    orderId = db.Column(db.Integer, db.ForeignKey(
+        'order.id'), nullable=True)
     __mapper_args__ = {
         'polymorphic_identity': 'payment',
         'polymorphic_on': payment_mode
     }
-    orderId = db.Column(db.Integer, db.ForeignKey(
-        'order.id'), nullable=True)
-    order = db.relationship(
-        'order', backref='payment', foreign_keys=[orderId])

af6871365080f05247ccfd0bb2439d1f39514713
Abigail002 <abigailameogno98@gmail.com>
2023-02-16 09:34:44 +0000

    Add of a relationship between order and payment

--- models/Order.py
+++ models/Order.py
@@ -2,10 +2,12 @@ from config import db
 """ from models import Customer
  """
 
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    createDate = db.Column(db.Date, nullable=False)
+    createDate = db.Column(db.DateTime, nullable=False)
     customerId = db.Column(db.Integer, db.ForeignKey(
         'customer.id'), nullable=True)
     customer = db.relationship(
         'Customer', backref='order', foreign_keys=[customerId])
+    payment = db.relationship('Payment', backref='order', uselist=False)
